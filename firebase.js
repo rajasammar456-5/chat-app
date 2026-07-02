2fec788d5b41cbbef08a42fddf060f75c75798e3
rajasammar456-5 <rajasmmar4@gmail.com>
2026-07-02 21:46:57 +0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBlNKFNnZEeK_NVf2-XMEfQgBP_IghKlyU",
